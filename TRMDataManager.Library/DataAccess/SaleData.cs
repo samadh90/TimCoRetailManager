17910f87014ab2c1b174b78f7656ce1000029fa9
Samad Hatsijev <crysis90war@hotmail.be>
2020-05-23 13:39:54 +0200

    Fix Missing Tax Rate
    Added back the tax rate to the config and added an API endpoint for getting the tax rate.

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +14,36 @@ namespace TRMDataManager.Library.DataAccess
     {
         private readonly IProductData _productData;
         private readonly ISqlDataAccess _sql;
+        private readonly IConfiguration _config;
 
-        public SaleData(IProductData productData, ISqlDataAccess sql)
+        public SaleData(IProductData productData, ISqlDataAccess sql, IConfiguration config)
         {
             _productData = productData;
             _sql = sql;
+            _config = config;
+        }
+
+        public decimal GetTaxRate()
+        {
+            string rateText = _config.GetValue<string>("TaxRate");
+
+            bool isValidTaxRate = Decimal.TryParse(rateText, out decimal output);
+
+            if (isValidTaxRate == false)
+            {
+                throw new ConfigurationErrorsException("The tax rate is not set up properly");
+            }
+
+            output = output / 100;
+
+            return output;
         }
 
         public void SaveSale(SaleModel saleInfo, string cashierId)
         {
             List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
 
-            var taxeRate = ConfigHelper.GetTaxRate() / 100;
+            var taxeRate = GetTaxRate();
 
             foreach (var item in saleInfo.SaleDetails)
             {
